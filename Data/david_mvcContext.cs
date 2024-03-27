4df0f6fe299b57b3b54d7d83cb82f57bdbfd9da4
Czeglédi Dávid <94075938+czegledi-david@users.noreply.github.com>
2024-03-27 23:23:25 +0100

    Add files via upload

---
+++ Data/david_mvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace david_mvc.Data
+{
+    public class david_mvcContext : DbContext
+    {
+        public david_mvcContext (DbContextOptions<david_mvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; } = default!;
+    }
+}
